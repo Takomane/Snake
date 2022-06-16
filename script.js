982e1652e84ff50919bc5f347e7ec51cee1a242a
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-06-15 21:04:53 -0400

    keydown event

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const canvas = document.querySelector('canvas'); //set canvas
 const ctx = canvas.getContext('2d'); //Set the context to 2d
 canvas.width = 800 ; //Set the width of the canvas
 canvas.height = 600; //Set the height of the canvas
+let speed = 7; //Set the speed of the snake
+let xVelocity = 0;
+let yVelocity = 0;
 
+function drawGame() { //Draw the game
+    setTimeout(drawGame, 1000/ Speed);
+    changePlayerPosition();
+}
 class Sprite { //class to use for the snake and food
     constructor(position) { //arguement for position of the sprite for the individual ones
         this.position = position; //position of the sprite
@@ -25,3 +32,16 @@ const food = new Sprite({ //creates the food
 });
 
 food.draw() //calls the draw function to the food
+
+function changePlayerPosition() { //function to change the position of the player
+    player.position.x += xVelocity;
+    player.position.y += yVelocity;
+}
+
+document.body.addEventListener('keydown', keyDown);
+function keyDown(event){
+    if(event.keyCode === 38){   //up
+        xVelocity = 0;
+        yVelocity = -1;
+    }
+}
